22de4a24a59722f0fe2b271bfe1c2e3baea3c95c
ErickVasquezz <89364442+ErickVasquezz@users.noreply.github.com>
2025-05-10 17:26:08 -0400

    fix: vite.config

--- landing-iman/vite.config.js
+++ landing-iman/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base:'https://Iman-Proyect.github.io/LandingPage-Iman/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

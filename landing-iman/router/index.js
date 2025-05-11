ea22a08fd751985cf53272ae42609caa6314fd1d
ErickVasquezz <89364442+ErickVasquezz@users.noreply.github.com>
2025-05-10 20:13:57 -0400

    fix: adjust route to work in github pages

--- landing-iman/router/index.js
+++ landing-iman/router/index.js
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory('/LandingPage-Iman/'),
     routes,
     scrollBehavior(to) {
         if (to.hash) {
